935e2406b32b560426604a6f47f027cf06b578ac
santiagoAM2004 <sam2454221@gmail.com>
2021-10-12 01:49:34 -0500

    add the file Readme.js with basic commands

---
+++ js/Readme.js
@@ -0,0 +1,23 @@
+//connect JS with HTML
+
+//<script type = "XXXXX"> </script>
+//<script src = "file's address"> </script>
+
+//kind of data
+let string = "Santiago";
+let number = 23;
+let boolean = false; // or true
+let nullVariable = null;
+undefined //could be when a variable is declared but isn't inicializd
+let firtsVariable = 2, secondVariable = 3; // you can do that, declared and inicializec in the same line
+
+// declare a variable 
+var date = 11; // global range
+
+const numberPi = 3.1416; // the variable can't be change during the program
+//const have to declared and inicialized in the same line
+
+let hour = "23:34"; // limit range 
+
+alert("message")
+let message = prompt("introduce a message:") //that is equivalent to an "input" in Python
