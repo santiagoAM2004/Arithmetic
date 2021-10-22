24f072e7c7705738542732c52d752ba3105ee200
santiagoAM2004 <sam2454221@gmail.com>
2021-10-22 00:08:18 -0500

    add file's of C#(console and ASP) in visual studio code

---
+++ C#/BubbleSort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = new List<int>();
+
+            Random random = new Random();
+
+            Console.WriteLine("type the range of random's number:");
+            int numberRange = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0;i < numberRange;i++)
+            {
+                int numberRandom = random.Next(-10,10);
+                Console.WriteLine(numberRandom);
+                numberList.Add(numberRandom);
+            }
+        }
+    }
+}
